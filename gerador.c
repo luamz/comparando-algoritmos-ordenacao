858c837803b0a177f77f66d099ff951f71bfb5db
VictoriaGranja <victoriagranja@id.uff.br>
2021-12-09 09:30:15 -0300

    escrita em arquivo

--- gerador.c
+++ gerador.c
@@ -1,18 +1,24 @@
 #include<stdio.h>
 #include <time.h>
 #include <stdlib.h>
+#include <string.h>
 
 void gera_sequencias(int n){
-    FILE *arquivo;
-    arquivo = fopen("sequencias.txt","w");
-
+    int num = 1;
+    char nome[8];
     srand(time(NULL));
         for (int j = 0 ; j < 10 ; j++){
+          FILE *arquivo;
+          sprintf(nome, "%d", num);
+          strcat(nome, ".txt");
+          arquivo = fopen(nome,"w");
             for (int k=0 ; k<n ; k++){
                 fprintf(arquivo, "%d,", rand()%1000);
+            }
+          fprintf(arquivo, "\n");
+          fclose(arquivo);
+          num += 1;
         }
-        fprintf(arquivo, "\n");
-    }
 }
 int main(){
     gera_sequencias(100);
