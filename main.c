c3f5b4edd6a5ddb164bff6789db39db23aead1bb
Luam <luamsilva@id.uff.br>
2021-12-09 15:03:29 -0300

    Organizando arquivos

--- main.c
+++ main.c
@@ -1,49 +1,31 @@
 #include<stdio.h>
-#include <unistd.h> 
-#include <sys/resource.h>
-#include "bolha.c"
-#include "caixas.c"
-#include "gerador.c"
-#include "merge.c"
-#include "insertion.c"
-
-void Tempo_CPU_Sistema(double *seg_CPU_total, double *seg_sistema_total)
-{
-  long seg_CPU, seg_sistema, mseg_CPU, mseg_sistema;
-  struct rusage ptempo;
-
-  getrusage(0,&ptempo);
-
-  seg_CPU = ptempo.ru_utime.tv_sec;
-  mseg_CPU = ptempo.ru_utime.tv_usec;
-  seg_sistema = ptempo.ru_stime.tv_sec;
-  mseg_sistema = ptempo.ru_stime.tv_usec;
-
- *seg_CPU_total     = (seg_CPU + 0.000001 * mseg_CPU);
- *seg_sistema_total = (seg_sistema + 0.000001 * mseg_sistema);
-}
+#include "metodos/bolha.c"
+#include "metodos/caixas.c"
+#include "metodos/merge.c"
+#include "metodos/insertion.c"
+#include "util.c"
 
 int main(){
     
-    double s_CPU_inicial, s_total_inicial, s_CPU_final, s_total_final;
-    
-    FILE *arqBolha = fopen("resultados/bolha.txt", "w");
-    double tempo_bolha;
+  double s_CPU_inicial, s_total_inicial, s_CPU_final, s_total_final;
+  
+  FILE *arqBolha = fopen("resultados/bolha.txt", "w");
+  double tempo_bolha;
 
-    FILE *arqCaixas = fopen("resultados/caixas.txt", "w");
-    double tempo_caixas;
+  FILE *arqCaixas = fopen("resultados/caixas.txt", "w");
+  double tempo_caixas;
 
-    FILE *arqMerge = fopen("resultados/merge.txt", "w");
-    double tempo_merge;
+  FILE *arqMerge = fopen("resultados/merge.txt", "w");
+  double tempo_merge;
 
-    FILE *arqInsertion = fopen("resultados/insertion.txt", "w");
-    double tempo_insertion;
+  FILE *arqInsertion = fopen("resultados/insertion.txt", "w");
+  double tempo_insertion;
 
-    for (int n=10 ; n<5000 ; n=n+10){
+  for (int n=10 ; n<1000 ; n=n+10){
       tempo_bolha = tempo_caixas = tempo_insertion = tempo_insertion = 0;
       int *vetor= (int*)malloc(sizeof(int)*n);
       gera_sequencias(n);
-      for (int nSeq = 1; nSeq < 6; nSeq++){
+      for (int nSeq = 1; nSeq < 10; nSeq++){
 
         vetor = leSequencia(nSeq, n);
 
@@ -76,4 +58,8 @@ int main(){
       escreve_resultado(arqInsertion,n,tempo_insertion);
     }
 
+  fclose(arqBolha);
+  fclose(arqCaixas);
+  fclose(arqMerge);
+  fclose(arqInsertion);
 }
